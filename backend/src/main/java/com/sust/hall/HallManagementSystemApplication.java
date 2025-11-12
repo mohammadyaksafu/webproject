e3c514b47e741c507f0ff26208a30dfedc926992
TusharOnPoint <tushar23das@gmail.com>
2025-11-12 14:05:10 +0600

    initial backend setup

---
+++ backend/src/main/java/com/sust/hall/HallManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sust.hall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HallManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HallManagementSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sust/hall/HallManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sust.hall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HallManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
